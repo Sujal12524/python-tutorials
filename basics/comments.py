e70d6c648ae9342a8815eed90870b55f3692d836
Sujal12524 <sujalsahil088@gmail.com>
2025-06-12 22:34:02 +0530

    Add comments to explain code functionality and calculations

---
+++ basics/comments.py
@@ -0,0 +1,20 @@
+# Learning how to use comments
+
+# This is a single-line comment
+# It explains what the next line does
+name = "Sujal"
+
+# Print a greeting
+print("Hello", name)
+
+"""
+This is a multi-line comment,
+also known as a docstring (used inside functions/classes).
+It can be used to explain code in more detail.
+"""
+
+# Let's do a simple calculation
+x = 10
+y = 5
+result = x + y # Adding two numbers
+print("Sum is:", result)
